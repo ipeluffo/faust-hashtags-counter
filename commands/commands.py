4d4bb20bee3c855d933212b719ef88e7b6267b8c
Ignacio Peluffo <ipeluffo@gmail.com>
2019-07-01 23:41:46 +0100

    Small refactor

--- commands/commands.py
+++ commands/commands.py
@@ -1,19 +1,17 @@
 import logging
-from typing import Optional
+from typing import List
 
 import peony
 from faust.cli import option
 from peony import PeonyClient
 
 from src.app import app, hashtags_topic
-from src.settings import CONSUMER_SECRET, CONSUMER_KEY, ACCESS_TOKEN_SECRET, ACCESS_TOKEN
+from src.settings import ACCESS_TOKEN, ACCESS_TOKEN_SECRET, CONSUMER_KEY, CONSUMER_SECRET
 
 logger = logging.getLogger(__name__)
 
 
-@app.command(
-    option("--track", type=str, help="List of words in CSV format to filter stream of tweets", required=True)
-)
+@app.command(option("--track", type=str, help="List of words in CSV format to filter stream of tweets", required=True))
 async def hashtags_events_generator(self, track: str):
     """
     Generate Kafka events for each hashtag found in tweets matching with the track used to filter the stream.
@@ -25,11 +23,8 @@ async def hashtags_events_generator(self, track: str):
 
     async with req as stream:
         async for tweet in stream:
-            if peony.events.tweet(tweet):
-                tweet_hashtags = tweet['entities']['hashtags']
-                for tweet_hashtag in tweet_hashtags:
-                    logger.info(f'Sending event for hashtag: {tweet_hashtag["text"].lower()}')
-                    await hashtags_topic.send(value=tweet_hashtag["text"].lower())
+            tweet_hashtags = _get_hashtags_from_tweet(tweet=tweet)
+            await _produce_events_for_hashtags(hashtags=tweet_hashtags)
 
 
 def _get_twitter_client() -> PeonyClient:
@@ -41,3 +36,18 @@ def _get_twitter_client() -> PeonyClient:
     )
 
     return PeonyClient(**creds)
+
+
+def _get_hashtags_from_tweet(tweet) -> List[str]:
+    tweet_hashtags = []
+
+    if peony.events.tweet(tweet):
+        tweet_hashtags = tweet['entities']['hashtags']
+
+    return tweet_hashtags
+
+
+async def _produce_events_for_hashtags(hashtags: List[str]) -> None:
+    for tweet_hashtag in hashtags:
+        logger.info(f'Sending event for hashtag: {tweet_hashtag["text"].lower()}')
+        await hashtags_topic.send(value=tweet_hashtag["text"].lower())
